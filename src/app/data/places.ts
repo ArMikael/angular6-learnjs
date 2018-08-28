5a4bb75744040b0c6630ad1417abcec052f9d35c
Michael Treser <armikael2@gmail.com>
2018-08-29 00:29:00 +0300

    Added interface for Place object.

---
+++ src/app/data/places.ts
@@ -0,0 +1,18 @@
+export interface IPlace {
+  img: string;
+  address: string;
+  phone: number;   // дополнительно задание pipe для форматирования
+  weather: {
+    title: string;
+    icon: string;
+    water: number;
+    temperature: number;
+  };
+  social_info: {
+    title: string;
+    img: string;
+    followers: number;
+    following: number;
+  };
+  type: string;
+}
