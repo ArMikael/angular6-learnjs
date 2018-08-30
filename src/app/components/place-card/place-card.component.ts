9beab47906ee8709b30f08dd30d0d039ee3bf385
ArMikael <armikael2@gmail.com>
2018-08-30 12:50:20 +0300

    Updated list of places. Binded data from places to HTML.

--- src/app/components/place-card/place-card.component.ts
+++ src/app/components/place-card/place-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IPlace } from '../../data/places';
 
 @Component({
   selector: 'app-place-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-card.component.css']
 })
 export class PlaceCardComponent implements OnInit {
+
+  @Input() public place: IPlace;
 
   constructor() { }
 
--- src/app/data/places.ts
+++ src/app/data/places.ts
@@ -24,7 +24,7 @@ export interface IPlace {
 const places: IPlace[] = [
   {
     img: 'assets/images/1.jpg',
-    address: 'Dan Panorama Eilat',
+    address: 'Dan Panorama, Eilat, Israel',
     phone: 9726389999,
     weather: {
       title: 'Hot and crazy',
@@ -41,8 +41,8 @@ const places: IPlace[] = [
     type: 'hotel'
   },
   {
-    img: 'assets/images/1.jpg',
-    address: 'Herods Palace',
+    img: 'http://lorempixel.com/500/300/nature/3',
+    address: 'Herods Palace, Eilat, Israel',
     phone: 9725435435,
     weather: {
       title: 'Very Hot',
@@ -52,11 +52,47 @@ const places: IPlace[] = [
     },
     social_info: {
       title: 'HerodsPalace',
-      img: 'assets/images/b1.jpg',
+      img: 'http://lorempixel.com/100/100/people/3/',
       followers: 32424,
       following: 436
     },
     type: 'hotel'
+  },
+  {
+    img: 'http://lorempixel.com/500/300/nature/4',
+    address: 'Palmachim Beach',
+    phone: 972111111,
+    weather: {
+      title: 'Very very Hot',
+      icon: 'hot',
+      water: 22,
+      temperature: 37
+    },
+    social_info: {
+      title: 'Palmachim',
+      img: 'http://lorempixel.com/100/100/people/4/',
+      followers: 23334,
+      following: 111
+    },
+    type: 'beach'
+  },
+  {
+    img: 'http://lorempixel.com/500/300/nature/5',
+    address: 'Mike\'s Place, Jerusalem, Israel',
+    phone: 9725435435,
+    weather: {
+      title: 'Nice Weather',
+      icon: 'cloud',
+      water: 20,
+      temperature: 33
+    },
+    social_info: {
+      title: 'MikesPlace',
+      img: 'http://lorempixel.com/100/100/people/5/',
+      followers: 5423,
+      following: 546
+    },
+    type: 'pub'
   }
 ];
 
