787c401618a1b076214b737f4e94795aba3fbf21
ArMikael <armikael2@gmail.com>
2018-08-30 17:50:11 +0300

    Removed nav-panel component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavPanelComponent } from './components/nav-panel/nav-panel.component';
 import { PlaceCardComponent } from './components/place-card/place-card.component';
 import { PlacesComponent } from './components/places/places.component';
 import { PlacesFilterPipe } from './pipes/places-filter.pipe';
@@ -10,7 +9,6 @@ import { PlacesFilterPipe } from './pipes/places-filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    NavPanelComponent,
     PlaceCardComponent,
     PlacesComponent,
     PlacesFilterPipe
