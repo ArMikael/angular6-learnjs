1ef71b93fffec7ef0338160b7da2084846f5b90e
ArMikael <armikael2@gmail.com>
2018-08-30 11:20:09 +0300

    Created basic data.

--- src/app/data/places.ts
+++ src/app/data/places.ts
@@ -1,3 +1,6 @@
+import {Observable, of} from 'rxjs';
+import {delay, map} from 'rxjs/internal/operators';
+
 export interface IPlace {
   img: string;
   address: string;
@@ -16,3 +19,49 @@ export interface IPlace {
   };
   type: string;
 }
+
+
+const places: IPlace[] = [
+  {
+    img: 'assets/images/1.jpg',
+    address: 'Dan Panorama Eilat',
+    phone: 9726389999,
+    weather: {
+      title: 'Hot and crazy',
+      icon: 'cloud',
+      water: 24,
+      temperature: 41
+    },
+    social_info: {
+      title: 'DanPanorama',
+      img: 'assets/images/b1.jpg',
+      followers: 2344,
+      following: 240
+    },
+    type: 'hotel'
+  },
+  {
+    img: 'assets/images/1.jpg',
+    address: 'Herods Palace',
+    phone: 9725435435,
+    weather: {
+      title: 'Very Hot',
+      icon: 'cloud',
+      water: 26,
+      temperature: 39
+    },
+    social_info: {
+      title: 'HerodsPalace',
+      img: 'assets/images/b1.jpg',
+      followers: 32424,
+      following: 436
+    },
+    type: 'hotel'
+  }
+];
+
+export const places$: Observable<IPlace[]> = of(places)
+  .pipe(
+    map((placesArr: IPlace[]) => placesArr.filter((place: IPlace) => place.img)),
+    delay(1000)
+  );
