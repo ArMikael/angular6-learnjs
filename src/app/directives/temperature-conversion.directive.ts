63f995421b392e0150458f55f381144e956dd185
Michael Treser <armikael2@gmail.com>
2018-09-03 22:51:40 +0300

    Fixed Directive textContent. Added binding class to the Host.

--- src/app/directives/temperature-conversion.directive.ts
+++ src/app/directives/temperature-conversion.directive.ts
@@ -1,29 +1,27 @@
-import { Directive, OnInit, Input, ElementRef, Renderer2, HostListener } from '@angular/core';
+import {Directive, OnInit, Input, ElementRef, Renderer2, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appTemperatureConversion]'
 })
 export class TemperatureConversionDirective implements OnInit {
-  private _renderer: Renderer2;
-  private _elementRef: ElementRef;
-  
-  private _temperatureContext: HTMLSpanElement;
+  @HostBinding('class.temperature-section') public temperatureSection = true;
   
   @Input() public set appTemperatureConversion(temperature: string) {
     if (!temperature) { return; }
     
     const fahrenheit = parseInt(temperature) * 1.8 + 32;
-    
     this._temperatureContext.textContent = fahrenheit.toString();
   }
 
+  private _temperatureContext: HTMLSpanElement = this._renderer.createElement('span');
+
   // @HostListener('mouseover') 
 
-  constructor() { }
+constructor(private _renderer: Renderer2,
+            private _elementRef: ElementRef) { }
   
   
-  ngOnInit() {
-    this._temperatureContext = this._renderer.createElement('span');
+  public ngOnInit() {
     this._temperatureContext.className = 'fahrenheit-tooltip';
     this._elementRef.nativeElement.appendChild(this._temperatureContext);
   }
