82d2747dd8724d28af568ab4d27b3751ca10776e
Michael Treser <armikael2@gmail.com>
2018-09-08 11:27:49 +0300

    Added environment files and logic. Added StoreDevTools functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { PlaceCardComponent } from './components/place-card/place-card.component';
 import { PlacesComponent } from './components/places/places.component';
@@ -11,6 +13,7 @@ import { PlacesFilterPipe } from './pipes/places-filter.pipe';
 import { TemperatureConversionDirective } from './directives/temperature-conversion.directive';
 import { placesReducer } from "./store/reducers/places.reducer";
 import { PlacesEffects } from './store/effects/places.effect';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { PlacesEffects } from './store/effects/places.effect';
     BrowserModule,
     HttpClientModule,
     StoreModule.forRoot({ places: placesReducer }),
-    EffectsModule.forRoot([PlacesEffects])
+    EffectsModule.forRoot([PlacesEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [ ]
   ],
   providers: [],
   bootstrap: [AppComponent]
